a06799b6af75487c51ff834a4884f23dc7b64e30
Jonah Zukosky <jonah.zukosky@gmail.com>
2018-02-08 16:51:24 -0600

    Designed Prototype Cell and created Cell class

---
+++ AlarmClock/AlarmTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  AlarmTableViewCell.swift
+//  AlarmClock
+//
+//  Created by Jonah Zukosky on 2/8/18.
+//  Copyright © 2018 Zukosky, Jonah. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var meridianLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var repeatLabel: UILabel!
+    @IBOutlet weak var onOffSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
